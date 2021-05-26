174c102304dbded2362720340165ba65553984ae
nobody5050 <>
2021-05-26 05:42:26 +0000

    Changes

---
+++ code/exampleStrats/Final.py
@@ -0,0 +1,78 @@
+from decimal import Decimal
+import random
+
+import numpy as np
+
+def strategy(history, memory):
+	
+	# A finite state machine detective. 
+	# It tries to figure out what the oponent is thinking,
+	# and if it cant, it becomes a tit-for-tat
+	
+	num_rounds = history.shape[1] # number of rounds completed
+	max_defection_threshold = Decimal(1) / Decimal(2)  # do not forgive high defections
+	small_defection_window = 15
+	max_local_unprovoked_defections = 4  # too many unprovoked defections? random
+	window_start = max(0, num_rounds - small_defection_window) # Set start of detection window
+	window_end = num_rounds # end on our current round
+	opponents_recent_moves = history[1, window_start + 1 : window_end] #get opponents recent moves
+	our_recent_moves = history[0, window_start : window_end - 1] #get our recent moves
+	defections = opponents_recent_moves - our_recent_moves # subtract the moves from eachother to get a total number of defections
+	opponents_recent_defections = np.count_nonzero(defections == 1) # count the number of defections
+	testing_schedule = [1, 0, 0, 1, 1] # list of moves to perform while testing
+	
+	if memory == None:
+		if num_rounds == len(testing_schedule):
+			# Time to choose something.
+			opponent_moves = history[1]
+			if opponents_recent_defections > max_local_unprovoked_defections:
+				# Random Detected
+				choice = "defect"
+				memory = "alwaysDefect"
+			elif history[1, -1] == 1 and history[1, -2] == 1:
+				# they never defected, take advantage of them
+				choice = "defect"
+				memory = "defect-assuming-cooperative"
+			elif history[1, -1] == 0 and history[1, -2] == 0:
+				# they always defect
+				choice = "defect"
+				memory = "alwaysDefect"
+			elif opponent_moves[2] == 1 and opponent_moves[3] == 0:  
+				# ftft detected
+				choice = "cooperate"
+				memory = "alternate"
+			else:
+				choice = "cooperate"
+				memory = "tft"
+		if num_rounds >= len(testing_schedule):
+			# The game has gone on for longer than the testing schedule and we dont have a choice yet, choose tft
+			memory = "tft"
+		else:
+			# We havent picked something yet. We are in testing.
+			choice = testing_schedule[num_rounds]
+	else:
+		# We have a chosen state.
+		if memory == "defect-assuming-cooperative":
+			# always defect unless they defect
+			if opponents_recent_moves[1] == 0:
+				choice = "cooperate"
+				memory = "tft"
+			else:
+				choice = "defect"
+		elif memory == "tft":
+			#do tft
+			choice = "cooperate"
+			if history.shape[1] >= 1 and history[1,-1] == 0: 
+				# Choose to defect if and only if the opponent just defected.
+				choice = "defect"
+		elif memory == "alternate":
+			#alternate
+			our_last_move = history[0, -1] if num_rounds > 0 else 1
+			choice = 0 if our_last_move else 1
+		elif memory == "alwaysDefect":
+			#always defect
+			choice = "defect"
+		else:
+			choice = "cooperate"
+			
+	return choice, memory
