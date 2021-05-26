d34672466e2edf7ab17365adae73b87afe58f273
nobody5050 <>
2021-05-26 20:41:05 +0000

    Ok for real this time, final file

--- code/exampleStrats/Final.py
+++ code/exampleStrats/Final.py
@@ -8,11 +8,38 @@ def strategy(history, memory):
 	# A finite state machine detective. 
 	# It tries to figure out what the oponent is thinking,
 	# and if it cant, it becomes a tit-for-tat
+	#
+	# TODO list
+	# [X] Finite state machine
+	# [X] Behave like a tft while detecting
+	# [X] Handle randoms
+	# [?] Break out of D/D loops
+	# [ ] Detect fibbonachi (ahh how do you spell that)
+	# [ ] Maybe swap tft for another variant, unsure as of now
+	# [X] Submit
+	#
+	# Also, cary if you're reading this a few things:
+	# 1. I'd love to see another version if this contest 
+	# with a chance of misscomunication, 
+	# as well as some other highly requested features
+	#
+	# 2. Although I doubt i'll win, if I do then I want to explain
+	# more about this strategy. *please* contact me so i can
+	# explain it to you in more details than I can include in this
+	# simple readme.
+	#
+	# 3. Thanks for hosting such an amazing contest!
+	# I met so many cool people though this and the atmosphere
+	# inside the discord, with so many wonderful minds working
+	# together is truly phenominal. I'm not exagerating when I say
+	# that this week has truly changed my life.
+	#
+	# Thank you. 
 	
 	num_rounds = history.shape[1] # number of rounds completed
 	max_defection_threshold = Decimal(1) / Decimal(2)  # do not forgive high defections
-	small_defection_window = 15
-	max_local_unprovoked_defections = 4  # too many unprovoked defections? random
+	small_defection_window = 20
+	max_local_unprovoked_defections = 5 # too many unprovoked defections? random
 	window_start = max(0, num_rounds - small_defection_window) # Set start of detection window
 	window_end = num_rounds # end on our current round
 	opponents_recent_moves = history[1, window_start + 1 : window_end] #get opponents recent moves
@@ -22,13 +49,13 @@ def strategy(history, memory):
 	testing_schedule = [1, 0, 0, 1, 1] # list of moves to perform while testing
 	
 	if memory == None:
-		if num_rounds == len(testing_schedule):
+		if num_rounds >= len(testing_schedule):
 			# Time to choose something.
 			opponent_moves = history[1]
 			if opponents_recent_defections > max_local_unprovoked_defections:
 				# Random Detected
 				choice = "defect"
-				memory = "alwaysDefect"
+				memory = "defect-assuming-random"
 			elif history[1, -1] == 1 and history[1, -2] == 1:
 				# they never defected, take advantage of them
 				choice = "defect"
@@ -44,13 +71,10 @@ def strategy(history, memory):
 			else:
 				choice = "cooperate"
 				memory = "tft"
-		if num_rounds >= len(testing_schedule):
-			# The game has gone on for longer than the testing schedule and we dont have a choice yet, choose tft
-			memory = "tft"
 		else:
 			# We havent picked something yet. We are in testing.
 			choice = testing_schedule[num_rounds]
-	else:
+	if memory != None:
 		# We have a chosen state.
 		if memory == "defect-assuming-cooperative":
 			# always defect unless they defect
@@ -72,7 +96,16 @@ def strategy(history, memory):
 		elif memory == "alwaysDefect":
 			#always defect
 			choice = "defect"
+		elif memory == "defect-assuming-random":
+			# always defect unless they defect thrice in a row, indicating tft behavior (joss)
+			choice = "defect"
+			if history[1, -1] == 0 and history[1, -2] == 0 and history[1, -3] == 0:
+				choice = "cooperate"
+				memory = "alwaysCooperate"
+		elif memory == "alwaysCooperate":
+			choice = "cooperate"
 		else:
 			choice = "cooperate"
-			
+	
+	print(memory)		
 	return choice, memory
